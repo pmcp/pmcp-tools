385a9f7bf19c4b152528c8593a9bf6734b5fa172
pmcp <maartenlauwaert@gmail.com>
2020-01-14 15:57:57 +0100

    :wrench: Add firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: "AIzaSyAfUQQc7YYj8tw_3MCSy_mrajj1VyyvGgk",
+  authDomain: "pmcp-tools.firebaseapp.com",
+  databaseURL: "https://pmcp-tools.firebaseio.com",
+  projectId: "pmcp-tools",
+  storageBucket: "pmcp-tools.appspot.com",
+  messagingSenderId: "754653235294",
+  appId: "1:754653235294:web:3d3359c4be848fb2810ccf"
 }
 
 firebase.initializeApp(config)
